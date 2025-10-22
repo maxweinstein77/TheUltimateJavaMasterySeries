db863c5db62b53237937d2b5704bda35539cbee3
mweinstein7 <maxw86343@gmail.com>
2025-10-21 23:59:31 -0500

    Day 9

---
+++ Tutorial/src/com/mweinstein/Main.java
@@ -0,0 +1,10 @@
+package com.mweinstein;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
+    }
+}
