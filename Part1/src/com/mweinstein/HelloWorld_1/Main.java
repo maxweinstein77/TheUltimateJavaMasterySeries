9ee769da8102c3ec442de34a1c3d0f909c003a59
mweinstein7 <maxw86343@gmail.com>
2025-11-06 20:44:27 -0600

    day 24

---
+++ Part1/src/com/mweinstein/HelloWorld_1/Main.java
@@ -0,0 +1,18 @@
+package com.mweinstein.HelloWorld_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+/*
+To compile and execute program, open terminal:
+1) cd src/com/mweinstein/HelloWorld
+2) javac Main.java
+3) cd ..
+4) cd ..
+5) cd ..
+6) java com.mweinstein.HelloWorld.Main
+ */
---
+++ Part1/src/com/mweinstein/Variables_2/Main.java
@@ -0,0 +1,17 @@
+package com.mweinstein.Variables_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 30;
+        System.out.println(age);
+
+        int myAge = 35;
+        int herAge = myAge;
+        System.out.println(herAge);
+    }
+}
+
+/*
+To run program, press ctrl + R
+ */
