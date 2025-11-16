4538301876584d63115ad16d15153f20ae33ac90
mweinstein7 <maxw86343@gmail.com>
2025-11-15 23:43:28 -0600

    day 33

---
+++ Part2/src/com/mweinstein/p4_simplifyingifstatements/Main.java
@@ -0,0 +1,19 @@
+package com.mweinstein.p4_simplifyingifstatements;
+
+public class Main {
+
+   public static void main(String[] args) {
+
+      // Okay version
+      int income1 = 120_000;
+      boolean hasHighIncome1;
+      if (income1 > 100_00)
+         hasHighIncome1 = true;
+      else
+         hasHighIncome1 = false;
+
+      // Better version
+      int income2 = 120_000;
+      boolean hasHighIncome2 = (income2 > 100_000);
+   }
+}
