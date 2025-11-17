cc7269e75d5aabfa47124c17779c42554e0d563b
mweinstein7 <maxw86343@gmail.com>
2025-11-16 20:40:42 -0600

    day 34

---
+++ Part2/src/com/mweinstein/p5_theternaryoperator/Main.java
@@ -0,0 +1,25 @@
+package com.mweinstein.p5_theternaryoperator;
+
+public class Main {
+
+   public static void main(String[] args) {
+
+      // Amateur version
+      int income1 = 120_000;
+      String className1;
+      if (income1 > 100_000)
+         className1 = "First";
+      else
+         className1 = "Economy";
+
+      // Better version
+      int income2 = 120_000;
+      String className2 = "Economy";
+      if (income2 > 100_000)
+         className2 = "First";
+
+      // Even better version
+      int income3 = 120_000;
+      String className3 = income3 > 100_000 ? "First" : "Economy";
+   }
+}
